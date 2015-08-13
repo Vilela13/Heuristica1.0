f7907c4d774da27d008bc10b72a2e849a2d8105e
Mavus <mateusvilelasouza@gmail.com>
2015-08-13 13:04:08 -0300

    Primeiro commit

---
+++ Debug/src/Heuristica1.0.d
@@ -0,0 +1,1 @@
+src/Heuristica1.0.d: ../src/Heuristica1.0.cpp
---
+++ src/Heuristica1.0.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 0.cpp
+// Author      : Mateus Vilela Souza
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Galo Doido!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
